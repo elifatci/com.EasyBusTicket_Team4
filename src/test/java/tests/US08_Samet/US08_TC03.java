76bc9392830248df5c9ddb6158c8b1c40999d211
Samet Yıldırım <smt03y@gmail.com>
2024-01-16 21:03:12 +0300

    Samet/16/01/Kodlamalara devam

--- src/test/java/tests/US08_Samet/US08_TC03.java
+++ src/test/java/tests/US08_Samet/US08_TC03.java
@@ -1,21 +1,66 @@
 package tests.US08_Samet;
 
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import pages.UserDashboard;
+import pages.VisitorHomepage;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.ReusableMethods;
 
 public class US08_TC03 {
-    @Test public void test3() {
-//   Navigate to "https://qa.easybusticket.com/contact" which is the Contact page of the site
-//   Verify the visibility of the contact form named 'Have any Questions? on the 'Contact' page
-//   Check if the 'name' field is pre-filled for registered users.
-//   Enter an appropriate data into the 'Name' field in the contact form.
-//    Check if the 'email' field is pre-filled for registered users.
-//   Enter an appropriate data into the 'Email' field in the contact form.
-//   Enter an appropriate data into the 'Subject' field in the contact form.
-//   Enter an appropriate data into the 'message' field in the contact form.
-//   Verify the visibility of the button named 'Send Us Message' on the contact form
-//   Click the button named 'Send Us Message'
-//   Verify that the alert message is "Your message has been sent. Thank you for contacting us."
-//   Verify that after sending the message, the page is navigated back to the 'Contact' page
-//   Verify that the message has been successfully sent and visible in the admin dashboard
-}
-}
+    @Test
+    public void test3() {
+        //   Navigate to "https://qa.easybusticket.com/contact" which is the Contact page of the site
+        Driver.getDriver().get(ConfigReader.getProperty("easyBusContactUrl"));
+        //   Verify the visibility of the contact form named 'Have any Questions? on the 'Contact' page
+        VisitorHomepage homepage = new VisitorHomepage();
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue(homepage.textHaveAnyQuestions.isDisplayed());
+        if (homepage.ButonCookieAccept.isDisplayed() && homepage.ButonCookieAccept.isEnabled()) {
+            homepage.ButonCookieAccept.click();
+        //  Fill in the required fields
+        homepage.PlaceholderName.sendKeys("samet");
+        homepage.PlaceholderEmail.sendKeys("hjkh@kjhjkh.com");
+        homepage.PlaceholderSubject.sendKeys("Subject");
+        homepage.PlaceholderMessage.sendKeys("This is my message");
+        //  Click the Send Us Message button
+        ReusableMethods.clickWithJS(homepage.ButtonSendUsMessage);
+        ReusableMethods.wait(2);
+        //  Verify that the alert message is "Your message has been sent. Thank you for contacting us."
+        String actualMessage = homepage.messageYourmessagehasbeensent.getText();
+        String expectedMessage = "Your message has been sent. Thank you for contacting us.";
+        softAssert.assertEquals(expectedMessage, actualMessage);
+        //  Click the Sign İn button
+         ReusableMethods.clickWithJS(homepage.ButtonSignIn);
+        // Fill in the required fields.
+           homepage.textBoxUsername.sendKeys("Tetaci");
+           homepage.textBoxPassword.sendKeys("Sa.1234");
+        // Click the Log In button
+            homepage.buttonLogin.click();
+            ReusableMethods.wait(2);
+            UserDashboard pageUser= new UserDashboard();
+        //  Click the Contact button
+            JavascriptExecutor jse = (JavascriptExecutor) Driver.getDriver();
+            jse.executeScript("arguments[0].scrollIntoViewIfNeeded(true);",pageUser.contactLinki);
+            ReusableMethods.clickWithJS(pageUser.contactLinki);
+            ReusableMethods.wait(2);
+        //  Fill in the required fields in the Have any questions section
+            homepage.PlaceholderSubject.sendKeys("Subject");
+            homepage.PlaceholderMessage.sendKeys("This is my message");
+        //  Click the button named 'Send Us Message'
+            ReusableMethods.clickWithJS(homepage.ButtonSendUsMessage);
+            ReusableMethods.wait(2);
+
+        //  Verify that the message has been successfully sent and visible in the admin dashboard
+            String actualMessageText= pageUser.messageText.getText();
+            softAssert.assertTrue(actualMessageText.contains("This is my message"));
+
+            softAssert.assertAll();
+            Driver.getDriver().close();
+
+    }
+}}
--- src/test/java/tests/US08_Samet/US08_TC04.java
+++ src/test/java/tests/US08_Samet/US08_TC04.java
@@ -1,57 +1,102 @@
 package tests.US08_Samet;
 
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import pages.VisitorHomepage;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.ReusableMethods;
 
 public class US08_TC04 {
     @Test
-            public void emailTetst(){
+    public void emailTetst() {
+    // 1 "https://qa.easybusticket.com/contact" adresine gidin
+        Driver.getDriver().get(ConfigReader.getProperty("easyBusContactUrl"));
+        VisitorHomepage homepage = new VisitorHomepage();
+        if (homepage.ButonCookieAccept.isDisplayed() && homepage.ButonCookieAccept.isEnabled()) {
+            homepage.ButonCookieAccept.click();
+    //2 İletişim formundaki 'Name' alanına herhangi bir veri girmeyin.
+    //3 Diğer alanlara uygun verileri girin (Email, Konu ve Mesaj).
+            ReusableMethods.wait(2);
+            homepage.buttonContact.click();
+            homepage.PlaceholderEmail.sendKeys("hjkh@kjhjkh.com");
+            homepage.PlaceholderSubject.sendKeys("Subject");
+            homepage.PlaceholderMessage.sendKeys("This is my wrong message");
+            String contactPageUrl = Driver.getDriver().getCurrentUrl();
+            //4 İletişim formundaki 'Send Us Message' adlı düğmeye tıklayın
+            ReusableMethods.clickWithJS(homepage.ButtonSendUsMessage);
+            ReusableMethods.wait(2);
+      //5 Mesajın iletilemediğini doğrulayın
+            SoftAssert softAssert = new SoftAssert();
+            String expectedMessage = "Your message has been sent. Thank you for contacting us.";
+            softAssert.assertNotEquals(homepage.messageYourmessagehasbeensent, expectedMessage);
+     //6 'Name' alanına uygun olmayan veriler girin Diğer alanlara uygun verileri girin.
+     //7 İletişim formundaki 'Send Us Message' adlı düğmeye tıklayın.
+            Actions actions = new Actions(Driver.getDriver());
+            ReusableMethods.wait(2);
 
-    // 1. "https://qa.easybusticket.com/contact" adresine git, bu siteye ait iletişim sayfasıdır.
-    // 2. 'Contact' sayfasında 'Have any Questions?' adlı iletişim formunun görünürlüğünü doğrula.
-    // 3. İletişim formundaki 'Name' alanına herhangi bir veri girmeyin.
-    // 4. Diğer alanlara uygun verileri girin (Email, Konu ve Mesaj).
-    // 5. İletişim formundaki 'Send Us Message' adlı düğmeye tıklayın.
-    // 6. Düğmeye tıklandığında 'Name' alanının altında 'Lütfen bu alanı doldurunuz' mesajının görüntülendiğini ve düğmenin etkisiz olduğunu doğrula.
-    // 7. 'Name' alanına uygun olmayan veriler girin (Bu alanda sadece boşluk karakterleriyle test edilmiştir. Sayılar ve özel karakterleri kabul edebildiği için bu tür bir girişle test yapılmamıştır).
-    // 8. İletişim formundaki 'Send Us Message' adlı düğmeye tıklayın.
-    // 9. Düğmeye tıklandıktan sonra, sayfanın sağ üst köşesinde "The name field is required" (İsim alanı zorunludur) hatası görüntülendiğini doğrula.
-    // 10. Hata mesajını aldıktan sonra sayfanın 'Contact' sayfasına geri döndüğünü doğrula.
-    // 11. Sayfaya geri dönüldükten sonra 'Name' alanı dışındaki diğer alanların önceki verilerle dolu olduğunu doğrula.
-    // 12. İletişim formundaki 'Email' alanına herhangi bir veri girmeyin.
-    // 13. Diğer alanlara uygun verileri girin (Name, Konu ve Mesaj).
-    // 14. İletişim formundaki 'Send Us Message' adlı düğmeye tıklayın.
-    // 15. Düğmeye tıklandığında 'Email' alanının altında 'Lütfen bu alanı doldurunuz' mesajının görüntülendiğini ve düğmenin etkisiz olduğunu doğrula.
-    // 16. 'Email' alanına uygun olmayan verileri girin.
-    // 17. Diğer alanlara uygun verileri girin (Name, Konu ve Mesaj).
-    // 18. İletişim formundaki 'Send Us Message' adlı düğmeye tıklayın.
-    // 19. Düğmeye tıklandığında 'Email' alanının altında 'Lütfen e-posta adresine bir "@" işareti ekleyin' mesajının görüntülendiğini ve düğmenin etkisiz olduğunu doğrula.
-    // 20. 'Email' alanına uygun olmayan verileri girin.
-    // 21. Diğer alanlara uygun verileri girin (Name, Konu ve Mesaj).
-    // 22. İletişim formundaki 'Send Us Message' adlı düğmeye tıklayın.
-    // 23. Düğmeye tıklandığında 'Email' alanının altında 'Lütfen "@" işaretinden sonra gelen kısmı ekleyin' mesajının görüntülendiğini ve düğmenin etkisiz olduğunu doğrula.
-    // 24. 'Email' alanına uygun olmayan verileri girin.
-    // 25. Diğer alanlara uygun verileri girin (Name, Konu ve Mesaj).
-    // 26. İletişim formundaki 'Send Us Message' adlı düğmeye tıklayın.
-    // 27. Düğmeye tıklandığında 'Email' alanının altında 'Lütfen "@" işaretinden sonra gelen kısma "." şeklinde alan adı ekleyin' mesajının görüntülendiğini ve düğmenin etkisiz olduğunu doğrula.
-    // 28. 'Subject' alanına herhangi bir veri girmeyin.
-    // 29. Diğer alanlara uygun verileri girin (Name, Email ve Mesaj).
-    // 30. İletişim formundaki 'Send Us Message' adlı düğmeye tıklayın.
-    // 31. Düğmeye tıklandığında 'Subject' alanının altında 'Lütfen bu alanı doldurunuz' mesajının görüntülendiğini ve düğmenin etkisiz olduğunu doğrula.
-    // 32. 'Subject' alanına uygun olmayan verileri girin.
-    // 33. Diğer alanlara uygun verileri girin (Name, Email ve Mesaj).
-    // 34. İletişim formundaki 'Send Us Message' adlı düğmeye tıklayın.
-    // 35. Düğmeye tıklandığında, sayfanın sağ üst köşesinde "The subject field is required" (Konu alanı zorunludur) hatası görüntülendiğini doğrula.
-    // 36. Hata mesajını aldıktan sonra sayfanın 'Contact' sayfasına geri döndüğünü doğrula.
-    // 37. Sayfaya geri dönüldükten sonra 'Subject' alanı dışındaki diğer alanların önceki verilerle dolu olduğunu doğrula.
-    // 38. İletişim formundaki 'Message' alanına herhangi bir veri girmeyin.
-    // 39. Diğer alanlara uygun verileri girin (Name, Email ve Konu).
-    // 40. İletişim formundaki 'Send Us Message' adlı düğmeye tıklayın.
-    // 41. Düğmeye tıklandığında 'Message' alanının altında 'Lütfen bu alanı doldurunuz' mesajının görüntülendiğini ve düğmenin etkisiz olduğunu doğrula.
-    // 42. 'Message' alanına uygun olmayan verileri girin.
-    // 43. İletişim formundaki 'Send Us Message' adlı düğmeye tıklayın.
-    // 44. Düğmeye tıklandığında, sayfanın sağ üst köşesinde "The message field is required" (Mesaj alanı zorunludur) hatası görüntülendiğini doğrula.
-    // 45. Hata mesajını aldıktan sonra sayfanın 'Contact' sayfasına geri döndüğünü doğrula.
-    // 46. Sayfaya geri dönüldükten sonra 'Message' alanı dışındaki diğer alanların önceki verilerle dolu olduğunu doğrula.
+            homepage.buttonContact.click();
+            ReusableMethods.wait(2);
+            homepage.PlaceholderName.sendKeys("     ");
+            homepage.PlaceholderEmail.sendKeys(Keys.CLEAR);
+            homepage.PlaceholderEmail.sendKeys("hjkh@kjhjkh.com");
+            homepage.PlaceholderSubject.sendKeys("Konusuz");
+            homepage.PlaceholderMessage.sendKeys("This is my wrong message");
+            ReusableMethods.clickWithJS(homepage.ButtonSendUsMessage);
+    //8  Mesajın iletilemediğini doğrulayın
+            softAssert.assertEquals(homepage.messageYourmessagehasbeensent.getText(), "The name field is required.");
+     //9  Hata mesajını aldıktan sonra sayfanın 'Contact' sayfasına geri döndüğünü doğrula.
+            softAssert.assertEquals(contactPageUrl, Driver.getDriver().getCurrentUrl());
+     //10  Sayfaya geri dönüldükten sonra 'Name' alanı dışındaki diğer alanların önceki verilerle dolu olduğunu doğrula.
+            ReusableMethods.wait(2);
+            softAssert.assertEquals(homepage.PlaceholderEmail.getText(), "hjkh@kjhjkh.com");
+            softAssert.assertEquals(homepage.PlaceholderSubject.getText(), "Konusuz");
+            softAssert.assertEquals(homepage.PlaceholderMessage.getText(), "This is my wrong message");
+     //11  İletişim formundaki 'Email' alanına herhangi bir veri girmeyin.
+     //12  Diğer alanlara uygun verileri girin
+     //13  İletişim formundaki 'Send Us Message' adlı düğmeye tıklayın.
+           // ReusableMethods.wait(2);
+           // homepage.PlaceholderName.sendKeys("ayse");
+           // homepage.PlaceholderEmail.sendKeys(Keys.CLEAR);
 
+
+
+    //14  Mesajın iletilemediğini doğrulayın
+
+     //14  'Email' alanına uygun olmayan verileri girin.
+     //15  Diğer alanlara uygun verileri girin (Name, Konu ve Mesaj).
+     //16  İletişim formundaki 'Send Us Message' adlı düğmeye tıklayın.
+     //17  Düğmeye tıklandığında 'Email' alanının altında 'Lütfen e-posta adresine bir "@" işareti ekleyin' mesajının görüntülendiğini ve düğmenin etkisiz olduğunu doğrula.
+     //18  'Email' alanına uygun olmayan verileri girin.
+     //19  Diğer alanlara uygun verileri girin (Name, Konu ve Mesaj).
+     //20  İletişim formundaki 'Send Us Message' adlı düğmeye tıklayın.
+     //  Düğmeye tıklandığında 'Email' alanının altında 'Lütfen "@" işaretinden sonra gelen kısmı ekleyin' mesajının görüntülendiğini ve düğmenin etkisiz olduğunu doğrula.
+     //  'Email' alanına uygun olmayan verileri girin.
+     //  Diğer alanlara uygun verileri girin (Name, Konu ve Mesaj).
+     //  İletişim formundaki 'Send Us Message' adlı düğmeye tıklayın.
+     //  Düğmeye tıklandığında 'Email' alanının altında 'Lütfen "@" işaretinden sonra gelen kısma "." şeklinde alan adı ekleyin' mesajının görüntülendiğini ve düğmenin etkisiz olduğunu doğrula.
+     //  'Subject' alanına herhangi bir veri girmeyin.
+     //  Diğer alanlara uygun verileri girin (Name, Email ve Mesaj).
+     //  İletişim formundaki 'Send Us Message' adlı düğmeye tıklayın.
+     //  Düğmeye tıklandığında 'Subject' alanının altında 'Lütfen bu alanı doldurunuz' mesajının görüntülendiğini ve düğmenin etkisiz olduğunu doğrula.
+     //  'Subject' alanına uygun olmayan verileri girin.
+     //  Diğer alanlara uygun verileri girin (Name, Email ve Mesaj).
+     //  İletişim formundaki 'Send Us Message' adlı düğmeye tıklayın.
+     //  Düğmeye tıklandığında, sayfanın sağ üst köşesinde "The subject field is required" (Konu alanı zorunludur) hatası görüntülendiğini doğrula.
+     //  Hata mesajını aldıktan sonra sayfanın 'Contact' sayfasına geri döndüğünü doğrula.
+     //  Sayfaya geri dönüldükten sonra 'Subject' alanı dışındaki diğer alanların önceki verilerle dolu olduğunu doğrula.
+     //  İletişim formundaki 'Message' alanına herhangi bir veri girmeyin.
+     //  Diğer alanlara uygun verileri girin (Name, Email ve Konu).
+     //  İletişim formundaki 'Send Us Message' adlı düğmeye tıklayın.
+     //  Düğmeye tıklandığında 'Message' alanının altında 'Lütfen bu alanı doldurunuz' mesajının görüntülendiğini ve düğmenin etkisiz olduğunu doğrula.
+     //  'Message' alanına uygun olmayan verileri girin.
+     //  İletişim formundaki 'Send Us Message' adlı düğmeye tıklayın.
+     //  Düğmeye tıklandığında, sayfanın sağ üst köşesinde "The message field is required" (Mesaj alanı zorunludur) hatası görüntülendiğini doğrula.
+     //  Hata mesajını aldıktan sonra sayfanın 'Contact' sayfasına geri döndüğünü doğrula.
+     //  Sayfaya geri dönüldükten sonra 'Message' alanı dışındaki diğer alanların önceki verilerle dolu olduğunu doğrula.
+     softAssert.assertAll();
+        }
     }
 }
