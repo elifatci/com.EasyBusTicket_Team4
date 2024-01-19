a1b1fadec1a1715a19e5f7683b6cfe3e1a76bef6
Elif Atci <eelif0600@gmail.com>
2024-01-19 14:33:03 +0300

    Elif/19.01/report is done/

--- src/test/java/tests/US25_Omer/US25_TC04.java
+++ src/test/java/tests/US25_Omer/US25_TC04.java
@@ -6,27 +6,34 @@ import pages.AdminDashboard;
 import utilities.ConfigReader;
 import utilities.Driver;
 import utilities.ReusableMethods;
+import utilities.TestBaseRapor;
 
-public class US25_TC04 {
+public class US25_TC04 extends TestBaseRapor {
     @Test
     public void testUS25_01() {
+        extentTest=extentReports.createTest("Smoke test");
         //browser açılır
         //url "https://qa.easybusticket.com/admin"e gidilir.
         Driver.getDriver().get(ConfigReader.getProperty("adminUrl"));
         SoftAssert softAssert = new SoftAssert();
+        extentTest.info("AdminUrl sayfasina gidilir");
         // Doğru kullanıcı adı ve parola girilir.
         AdminDashboard adminDashboard = new AdminDashboard();
         adminDashboard.usernameKutusu.sendKeys("admin33");
         adminDashboard.passwordKutusu.sendKeys("123123123");
         //login butonuna tıklanır.
         adminDashboard.loginButonu.click();
+        extentTest.info("Gecerli bilgilerle giris yapilarak login butonuna tiklanir");
         //Payment History butonu tıklanır.
         ReusableMethods.clickWithJS(adminDashboard.paymentHistory);
+        extentTest.info("Payment History butonuna tiklanir");
         //Rejected Payment tıklanır.
         ReusableMethods.clickWithJS(adminDashboard.rejectedPayment);
+        extentTest.info("Rejected payment butonuna tiklanir");
         //https://qa.easybusticket.com/admin/payment/rejected sayfasına yönlendirildiği doğrulanır
         String expectedurl="https://qa.easybusticket.com/admin/payment/rejected";
         softAssert.assertEquals(Driver.getDriver().getCurrentUrl(),expectedurl,"Succesfullpayment sayfasına yönlendirilmedi");
+        extentTest.pass("Sayfaya erisildigi dogrulanir");
         ////rejected biletler action butonu tıklanır.
         adminDashboard.paymentHistoryActionLinkii.click();
         //UserdepositinformationText görüntülendiği doğrulanır.
